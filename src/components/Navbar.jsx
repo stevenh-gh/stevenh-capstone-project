9d92cf34f4d795327887d144bd958779d5882f03
steven huynh <steven.h@live.com>
2024-04-01 18:10:24 -0700

    convert navbar list to horizontal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,29 @@
+import { Box, Typography } from "@mui/material";
+
 import { Link } from "react-router-dom";
-import { Typography } from "@mui/material";
 
 function Navbar({ token, setToken }) {
   return (
-    <Typography variant="body1">
-      <nav>
-        <ul>
-          <li>
+    <nav>
+      <Typography variant="body1">
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box>
             <Link to="/">Home</Link>
-          </li>
-          <li>
+          </Box>
+          <Box>
             {!token
-              ? <Link to="/login">login</Link>
-              : (
-                <span
-                  onClick={() => {
-                    window.localStorage.setItem("token", "");
-                    setToken(window.localStorage.getItem("token"));
-                  }}
-                >
-                  logout
-                </span>
-              )}
-          </li>
-          {!token && (
-            <li>
-              <Link to="/signup">Sign up</Link>
-            </li>
-          )}
-          {token && (
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-          )}
-        </ul>
-      </nav>
-    </Typography>
+              ? (
+                <>
+                  <Link to="/signup">Sign up</Link>
+                  <Link to="/login">Login</Link>
+                </>
+              )
+              : <Link onClick={() => setToken(null)}>Logout</Link>}
+            {token && <Link to='/cart'>Cart</Link>}
+          </Box>
+        </Box>
+      </Typography>
+    </nav>
   );
 }
 
