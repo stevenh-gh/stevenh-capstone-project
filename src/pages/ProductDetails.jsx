1c64844cb815e180f6b70eaa58ef397ad3353f83
steven huynh <steven.h@live.com>
2024-04-04 20:52:34 -0700

    add add-to-cart for product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import { AddShoppingCart } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import { getProduct } from "../api";
 import { useParams } from "react-router-dom";
 
@@ -23,6 +25,7 @@ function ProductDetails() {
           <h3>{product.title} - {product.rating.rate}/5 ({product.rating.count})</h3>
           <h4>${product.price}</h4>
           <p>{product.description}</p>
+          <Button onClick={() => console.log(id)} variant="contained" size="large">Add to cart <AddShoppingCart sx={{ marginLeft: '10px' }} /></Button>
         </>
       )}
     </>
