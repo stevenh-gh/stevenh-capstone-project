ae8b595574b50165b38f67d5b326d37e284863ae
steven huynh <steven.h@live.com>
2024-04-05 19:22:16 -0700

    make add item to localStorage cart from product details page work

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { AddShoppingCart } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { addToCart } from "../helpers";
 import { getProduct } from "../api";
 import { useParams } from "react-router-dom";
 
@@ -25,7 +26,7 @@ function ProductDetails() {
           <h3>{product.title} - {product.rating.rate}/5 ({product.rating.count})</h3>
           <h4>${product.price}</h4>
           <p>{product.description}</p>
-          <Button onClick={() => console.log(id)} variant="contained" size="large">Add to cart <AddShoppingCart sx={{ marginLeft: '10px' }} /></Button>
+          <Button onClick={() => addToCart(product.id)} variant="contained" size="large">Add to cart <AddShoppingCart sx={{ marginLeft: '10px' }} /></Button>
         </>
       )}
     </>
