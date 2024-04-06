06baafb18256d14ba2235e8296648b1d65e500f3
steven huynh <steven.h@live.com>
2024-04-05 19:13:18 -0700

    add addToCart helper function

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+export function addToCart(id) {
+  let cart = window.localStorage.getItem('cart')
+  cart = JSON.parse(cart)
+  let cartProds = cart.products
+  let prodObj = cartProds.find(prod => prod.productId === id);
+  if (prodObj) {
+    prodObj.quantity++;
+  }
+  else {
+    cartProds.push({ productId: id, quantity: 1 });
+  }
+  window.localStorage.setItem('cart', JSON.stringify(cart))
+}
