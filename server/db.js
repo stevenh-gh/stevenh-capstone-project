2ace049b6ba2030d24ae44fa9d700bca78069ef2
steven huynh <steven.h@live.com>
2024-04-10 21:44:22 -0700

    add category and product functions

--- server/db.js
+++ server/db.js
@@ -7,10 +7,27 @@ const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/e
 const createTables = async () => {
   const sql = `
     drop table if exists "user";
+    drop table if exists product;
+    drop table if exists category;
+
     create table "user"(
       id uuid primary key,
       username varchar(255) unique not null,
       password varchar(255) not null
+    );
+
+    create table category(
+      id serial primary key,
+      name varchar(255)
+    );
+
+    create table product(
+      id uuid primary key,
+      title varchar(255) not null,
+      price decimal not null,
+      category_id integer references category(id) not null,
+      description varchar(255) not null,
+      image varchar(255) not null
     );
   `
   await client.query(sql)
@@ -36,9 +53,52 @@ const getAllUsers = async () => {
   return response.rows
 }
 
+const createProduct = async ({ title, price, category, description, image }) => {
+  const sql = `
+    insert into product
+      (id, title, price, category_id, description, image)
+    values
+      ($1, $2, $3, $4, $5, $6)
+    returning *;
+  `
+  const response = await client.query(sql, [uuid(), title, price, category, description, image])
+  return response[0]
+}
+
+const createCategory = async (name) => {
+  const sql = `
+    insert into category
+      (name)
+    values
+      ($1);
+  `
+  await client.query(sql, [name])
+}
+
+const getAllCategories = async () => {
+  const sql = `
+    select * from category;
+  `
+  const response = await client.query(sql)
+  return response.rows
+}
+
+const getAllProducts = async () => {
+  const sql = `
+    select * , category.name as category from product  
+    join category on product.category_id = category.id;
+  `
+  const response = await client.query(sql)
+  return response.rows
+}
+
 export {
   client,
   createTables,
   createUser,
   getAllUsers,
+  createCategory,
+  createProduct,
+  getAllCategories,
+  getAllProducts,
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import { client, createTables, createUser, getAllUsers } from './db.js';
+import { client, createCategory, createProduct, createTables, createUser, getAllCategories, getAllProducts, getAllUsers } from './db.js';
 
 import express from 'express';
 import morgan from 'morgan'
@@ -17,6 +17,22 @@ app.get('/users', async (req, res, next) => {
   }
 })
 
+app.get('/products/categories', async (req, res, next) => {
+  try {
+    res.send(await getAllCategories());
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.get('/products', async (req, res, next) => {
+  try {
+    res.send(await getAllProducts())
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 const init = async () => {
   await client.connect()
   console.log('connected to db')
@@ -28,6 +44,23 @@ const init = async () => {
   console.log('creating users...')
   await createUser({ username: 'johnd', password: 'm38rmF$' })
   console.log('users created')
+
+  console.log('creating categories...')
+  await createCategory('electronics')
+  await createCategory('jewelry')
+  await createCategory('men\'s clothing')
+  await createCategory('women\'s clothing')
+  console.log('categories created')
+
+  console.log('creating products...')
+  await createProduct({
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: 3,
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  })
+  console.log('products created')
 
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
