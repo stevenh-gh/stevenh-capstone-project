4e1c4161cb35d45178875e0dcaeb597b0dc1df47
steven huynh <steven.h@live.com>
2024-04-10 19:35:37 -0700

    add createTables function

--- server/db.js
+++ server/db.js
@@ -2,6 +2,19 @@ import pg from 'pg'
 
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/e_commerce_db')
 
+const createTables = async () => {
+  const sql = `
+    drop table if exists users;
+    create table users(
+      id uuid primary key,
+      username varchar(255) unique not null,
+      password varchar(255) not null
+    );
+  `
+  await client.query(sql)
+}
+
 export {
-  client
+  client,
+  createTables,
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import { client } from './db.js';
+import { client, createTables } from './db.js';
+
 import express from 'express';
 import morgan from 'morgan'
 import path from 'path'
@@ -9,6 +10,11 @@ app.use(morgan('dev'))
 
 const init = async () => {
   await client.connect()
+  console.log('connected to db')
+
+  console.log('creating tables...')
+  await createTables()
+  console.log('tables created')
 
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
