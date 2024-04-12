6d1d272d23a59a271bf0adb0d93e6cecfdf13542
steven huynh <steven.h@live.com>
2024-04-11 17:08:13 -0700

    add snackbar to productdetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,8 @@
+import { Button, Typography } from "@mui/material";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
 import { useEffect, useState } from "react";
 
 import { AddShoppingCart } from "@mui/icons-material";
-import { Button } from "@mui/material";
 import { addToCart } from "../helpers";
 import { getProduct } from "../api";
 import { useParams } from "react-router-dom";
@@ -18,18 +19,26 @@ function ProductDetails() {
     gp();
   }, []);
 
+  function handleClick(product) {
+    addToCart(product.id)
+    enqueueSnackbar(`Added ${product.title} to cart!`)
+  }
+
   return (
     <>
-      {product && (
-        <>
-          <img src={product.image} width={200} alt="" />
-          {/* <h3>{product.title} - {product.rating.rate}/5 ({product.rating.count})</h3> */}
-          <h3>{product.title}</h3>
-          <h4>${product.price}</h4>
-          <p>{product.description}</p>
-          <Button onClick={() => addToCart(product.id)} variant="contained" size="large">Add to cart <AddShoppingCart sx={{ marginLeft: '10px' }} /></Button>
-        </>
-      )}
+      <Typography>
+        {product && (
+          <>
+            <img src={product.image} width={200} height={200} alt="" />
+            {/* <h3>{product.title} - {product.rating.rate}/5 ({product.rating.count})</h3> */}
+            <h3>{product.title}</h3>
+            <h4>${product.price}</h4>
+            <p>{product.description}</p>
+            <Button onClick={() => handleClick(product)} variant="contained" size="large">Add to cart <AddShoppingCart sx={{ marginLeft: '10px' }} /></Button>
+            <SnackbarProvider />
+          </>
+        )}
+      </Typography>
     </>
   );
 }
