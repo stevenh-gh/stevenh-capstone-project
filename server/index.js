c1d83cb6bf10f977c281ebc9cd7f076c08785271
steven huynh <steven.h@live.com>
2024-04-11 22:43:36 -0700

    add deployment code

--- server/index.js
+++ server/index.js
@@ -15,15 +15,24 @@ import {
   getProduct
 } from './db.js';
 
+import { dirname } from 'path';
 import express from 'express';
+import { fileURLToPath } from 'url';
 import morgan from 'morgan'
 import path from 'path'
 import seedProducts from './seedProducts.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use(express.static(path.resolve(__dirname, '../dist/')))
+
+app.get('/', (req, res, next) => {
+  res.send(express.static(path.resolve(__dirname, '../dist/index.html')))
+})
 
 async function isLoggedIn(req, res, next) {
   try {
